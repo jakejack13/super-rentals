639e897c8c784f71cc435c01aa9e811103d444ea
Jacob Kerr <jck268@cornell.edu>
2025-06-19 13:25:51 -0400

    Fix website crashes (#253)

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -2,9 +2,17 @@
 
 const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
-module.exports = function (defaults) {
+module.exports = async function (defaults) {
   const app = new EmberApp(defaults, {
     // Add options here
+  });
+
+  const { setConfig } = await import('@warp-drive/build-config');
+  setConfig(app, __dirname, {
+    deprecations: {
+      DEPRECATE_TRACKING_PACKAGE: false,
+      DEPRECATE_STORE_EXTENDS_EMBER_OBJECT: false,
+    },
   });
 
   return app.toTree();
